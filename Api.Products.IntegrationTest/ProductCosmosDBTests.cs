eb5b10f22d5d47fea500f45f0b4bab069fa7413c
Scott Rutz <scrutz@microsoft.com>
2020-10-08 07:42:12 -0500

    cp

---
+++ Api.Products.IntegrationTest/ProductCosmosDBTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Api.Products.IntegrationTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
