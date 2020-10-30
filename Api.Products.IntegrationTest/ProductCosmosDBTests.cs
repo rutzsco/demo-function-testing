4541d3ea265356bb07f75b15e92080754081f9b0
Scott Rutz <scrutz@microsoft.com>
2020-10-30 11:48:55 -0500

    Update ProductCosmosDBTests.cs

--- Api.Products.IntegrationTest/ProductCosmosDBTests.cs
+++ Api.Products.IntegrationTest/ProductCosmosDBTests.cs
@@ -34,9 +34,10 @@ namespace Api.Products.IntegrationTest
 
 
 
-            _Client = new DocumentClient(new Uri("https://localhost:8081"), "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+            _Client = new DocumentClient(new Uri(accountEndpoint), accountKey);
+            _Client.OpenAsync().Wait();
 
-            var database = _Client.CreateDatabaseIfNotExistsAsync(new Database { Id = "ProductDatabase" });
+            _Client.CreateDatabaseIfNotExistsAsync(new Database { Id = "ProductDatabase" }).Wait();
             _Client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri("ProductDatabase")).Wait();
             CreatePartitionedCollection("ProductDatabase", "Products", "/id").Wait();
         }
