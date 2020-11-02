a107266236a446f227767370e852d99f02e2e1f9
Scott Rutz <scrutz@microsoft.com>
2020-11-02 11:58:20 -0600

    Update VerifyEndpoint.cs

--- Api.Products/VerifyEndpoint.cs
+++ Api.Products/VerifyEndpoint.cs
@@ -13,7 +13,7 @@ namespace Api.Products
     public static class VerifyEndpoint
     {
         [FunctionName("VerifyEndpoint")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "loaderio-f87f3bbfeae57b849bdda6ba5937d2e8")] HttpRequest req)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "loaderio-f87f3bbfeae57b849bdda6ba5937d2e8")] HttpRequest req)
         {
             return new OkObjectResult("loaderio-f87f3bbfeae57b849bdda6ba5937d2e8");
         }
