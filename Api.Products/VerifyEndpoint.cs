65f1a3af47b8072ecde3ddc24f3e9440b51ded72
Scott Rutz <scrutz@microsoft.com>
2020-11-02 11:01:09 -0600

    cp

---
+++ Api.Products/VerifyEndpoint.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Api.Products
+{
+    public static class VerifyEndpoint
+    {
+        [FunctionName("VerifyEndpoint")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "loaderio-f87f3bbfeae57b849bdda6ba5937d2e8")] HttpRequest req)
+        {
+            return new OkObjectResult("loaderio-f87f3bbfeae57b849bdda6ba5937d2e8");
+        }
+    }
+}
